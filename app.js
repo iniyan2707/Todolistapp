e876647c5a143c9ea186fcbee90940ad454d9b4f
iniyan2707 <iniyan.cool3@gmail.com>
2022-02-25 00:12:10 +0530

    added mongodb

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb+srv://admin-iniyan:Test123@cluster0.ieacd.mongodb.net/todolistDB', {useNewUrlParser:true});
+mongoose.connect('mongodb+srv://admin-iniyan:<password>@cluster0.ieacd.mongodb.net/todolistDB', {useNewUrlParser:true});
 
 const itemsSchema = {
   name : String,
